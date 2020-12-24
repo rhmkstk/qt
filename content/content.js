0df1f9c028803f3aa960d73437d72fce7b380f34
rhmkstk <rahmi.kstk@gmail.com>
2020-12-24 18:31:17 +0300

    take selected text from page, check if includes any letter, send to background with chrome api and take response comes from background(with second parameter))

--- content/content.js
+++ content/content.js
@@ -1,1 +1,13 @@
-console.log('this is content script')
+window.addEventListener("mouseup", starter);
+function starter() {
+  const selectedText = window.getSelection().toString();
+  if (isQueryTranslatable(selectedText)) {
+    chrome.runtime.sendMessage(selectedText, text => {
+      console.log(text);
+    });
+  }
+}
+
+function isQueryTranslatable(selected) {
+  return /[a-zA-Z]/g.test(selected)
+}
